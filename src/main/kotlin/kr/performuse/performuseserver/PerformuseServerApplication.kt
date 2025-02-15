43dc49c5c7387048bbd593e386b7723893c294d3
김종윤 <rlawhddbs@dgsw.hs.kr>
2025-02-15 17:38:48 +0900

    initial commit

---
+++ src/main/kotlin/kr/performuse/performuseserver/PerformuseServerApplication.kt
@@ -0,0 +1,11 @@
+package kr.performuse.performuseserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PerformuseServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<PerformuseServerApplication>(*args)
+}
